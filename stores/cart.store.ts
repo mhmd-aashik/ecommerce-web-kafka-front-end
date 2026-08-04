4b9ea4b1d8b9547221f1f7b338f04d12c56d421a
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-04 14:32:46 +0400

    Add zustand as a dependency in package.json and bun.lock

---
+++ stores/cart.store.ts
@@ -0,0 +1,39 @@
+"use client";
+
+import { Product } from "@/types/product";
+import { create } from "zustand";
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+interface CartState {
+  items: CartItem[];
+  // addItem: (product: Product) => void;
+  // removeItem: (productId: string) => void;
+  // updateQuantity: (productId: string, quantity: number) => void;
+  // clearCart: () => void;
+}
+
+export const useCartStore = create<CartState>((set) => ({
+  items: [],
+
+  addItem: (product: Product) =>
+    set((state) => {
+      const existingItem = state.items.find((item) => item.id === product.id);
+
+      if (existingItem) {
+        return {
+          items: state.items.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item,
+          ),
+        };
+      }
+
+      return {
+        items: [...state.items, { ...product, quantity: 1 }],
+      };
+    }),
+}));
